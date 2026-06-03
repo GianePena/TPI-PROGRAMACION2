f208c9cabfb165eb75a14ab33032c33a0041c778
marti06-stack <guadasuarez0506@gmail.com>
2026-06-03 20:41:24 -0300

    Hice las clases Pedido, DetallePedido,Interface Calculable, Enums: Estado, FormaPago y Rol

---
+++ src/main/java/entities/DetallePedido.java
@@ -0,0 +1,46 @@
+package entities;
+
+import java.time.LocalDateTime;
+
+public class DetallePedido extends Base{
+    //  cantidad, subtotal (y relación N:1 con Producto).
+   private int cantidad;
+   private double subtotal;
+   private Producto producto;
+
+   public DetallePedido() {
+       super();
+   }
+
+   public DetallePedido(int cantidad, double subtotal, Producto producto) {
+       super();
+       this.cantidad = cantidad;
+       this.subtotal = subtotal;
+       this.producto = producto;
+   }
+
+   public DetallePedido(Long id, int cantidad, double subtotal, boolean eliminado, LocalDateTime createdAt, Producto producto) {
+       super(id, eliminado, createdAt);
+       this.cantidad = cantidad;
+       this.subtotal = subtotal;
+       this.producto = producto;
+   }
+
+    public int getCantidad() {return cantidad;}
+    public void setCantidad(int cantidad) {this.cantidad = cantidad;}
+
+    public double getSubtotal() {return subtotal;}
+    public void setSubtotal(double subtotal) {this.subtotal = subtotal;}
+
+    public Producto getProducto() {return producto;}
+    public void setProducto(Producto producto) {this.producto = producto;}
+
+    @Override
+    public String toString() {
+        return "DetallePedido{" +
+                "cantidad=" + cantidad +
+                ", subtotal=" + subtotal +
+                ", producto=" + producto +
+                '}';
+    }
+}
